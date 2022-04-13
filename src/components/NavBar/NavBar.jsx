316cab515210699c4530ac3d62234cbf628298d6
Alvaro toth <alvaro.toth@hotmail.com>
2022-04-13 19:48:58 -0300

    navBar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './NavBar.css';
+
+
+function NavBar(props){
+    return(
+        <div className="nav">
+                <a href="#">
+                    <img className="logo" src="../logo.jpg" />
+                </a>  
+                <div className="navLinks">
+                    <a href="#">Inicio</a>
+                    <a href="#">Productos</a>
+                    <a href="#">Contacto</a>
+                </div>
+        </div>
+    );
+}
+
+export default NavBar;
